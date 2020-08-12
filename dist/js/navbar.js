7c2135caa8b14225b010c98c48561c8228fd1c6f
Laplace <johnalexisbuot@gmail.com>
2020-08-13 00:59:14 +0800

    Fixed bug for opening the navbar too fast, and when it stays open while transitioning to desktop view

--- dist/js/navbar.js
+++ dist/js/navbar.js
@@ -3,25 +3,54 @@ const dropDownMenu = document.querySelector(".navbar__items");
 const navbar = document.querySelector(".navbar");
 const header = document.getElementsByTagName("header");
 const body = document.getElementsByTagName("body");
+const vw = window.innerWidth;
 
 let navBarOpen = false;
 
-hamburger.addEventListener("click", async function () {
+hamburger.addEventListener("click", navBarHandler);
+
+async function navBarHandler() {
     if (!navBarOpen) {
-        hamburger.setAttribute("src", "./images/icon-close.svg");
-        body[0].setAttribute("class", "nav-active");
-        header[0].appendChild(dropDownMenu);
-
+        openNavBar();
+        hamburger.removeEventListener("click", navBarHandler);
         await new Promise((resolve) => setTimeout(resolve, 250));
-        dropDownMenu.classList.add("active");
+        hamburger.addEventListener("click", navBarHandler);
     } else {
-        dropDownMenu.classList.remove("active");
-        hamburger.setAttribute("src", "./images/icon-hamburger.svg");
+        closeNavBar();
+        hamburger.removeEventListener("click", navBarHandler);
         await new Promise((resolve) => setTimeout(resolve, 250));
-
-        body[0].removeAttribute("class");
-        navbar.appendChild(dropDownMenu);
+        hamburger.addEventListener("click", navBarHandler);
     }
 
     navBarOpen = !navBarOpen;
+}
+
+async function openNavBar() {
+    hamburger.setAttribute("src", "./images/icon-close.svg");
+    body[0].setAttribute("class", "nav-active");
+    header[0].appendChild(dropDownMenu);
+
+    await new Promise((resolve) => setTimeout(resolve, 250));
+    dropDownMenu.classList.add("active");
+}
+
+async function closeNavBar() {
+    dropDownMenu.classList.remove("active");
+    hamburger.setAttribute("src", "./images/icon-hamburger.svg");
+    await new Promise((resolve) => setTimeout(resolve, 250));
+
+    body[0].removeAttribute("class");
+    navbar.appendChild(dropDownMenu);
+}
+
+window.addEventListener("resize", function () {
+    if (vw >= 1280) {
+        dropDownMenu.classList.remove("active");
+        hamburger.setAttribute("src", "./images/icon-hamburger.svg");
+
+        body[0].removeAttribute("class");
+        navbar.appendChild(dropDownMenu);
+
+        navBarOpen = false;
+    }
 });
